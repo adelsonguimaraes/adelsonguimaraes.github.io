30c40c88229420f358e41644aebb4b2353219497
adelsonguimaraes <adelsonguimaraes@gmail.com>
2017-08-21 23:40:42 -0400

    gibis

---
+++ libs/js/code/code.js.js
@@ -0,0 +1,38 @@
+var gibis = [
+	{
+		id: 1,
+		título: 'Turma da Mônica',
+		descricao: 'As aventuras de cebolinha o luivo',
+		valor: 3.50
+	},
+	{
+		id: 2,
+		título: 'Star Wars',
+		descricao: 'O império contra-ataca',
+		valor: 8.30
+	},
+	{
+		id: 3,
+		título: 'O Rei Leão',
+		descricao: 'Um reino perdido',
+		valor: 4.50
+	},
+	{
+		id: 4,
+		título: 'Pinóquio',
+		descricao: 'A lenda da baleia',
+		valor: 10.00
+	},
+	{
+		id: 5,
+		título: 'Mickey e Donald',
+		descricao: 'A fantástica máquina do tempo',
+		valor: 5.90
+	},
+	{
+		id: 6,
+		título: 'Pateta e Max',
+		descricao: 'Embalos de sábado a tarde',
+		valor: 3.50
+	}
+];
