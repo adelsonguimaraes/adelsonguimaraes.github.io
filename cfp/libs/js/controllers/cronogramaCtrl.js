9ee8e1b62700a5feb20b328cb5659dc1c580fc9b
adelsonguimaraes <adelsonguimaraes@gmail.com>
2018-01-12 08:05:29 -0400

    .

--- cfp/libs/js/controllers/cronogramaCtrl.js
+++ cfp/libs/js/controllers/cronogramaCtrl.js
@@ -24,14 +24,14 @@ var cronogramaCtrl = function ($scope, $rootScope, $location, genericAPI) {
             {"nome":"Dezembro", 	"sigla":"DEZ", "ano":"0000"}
         ];
     
-        $scope.despesas = [
-            {"descricao":"Tênis", "valor":"90.00", "prestacoes":"5", "datavencimento":"2017-12-05"},
-            {"descricao":"Mouse Razor", "valor":"150", "prestacoes":"3", "datavencimento":"2018-01-05"},
-            {"descricao":"Carro", "valor":"450", "prestacoes":"25", "datavencimento":"2016-07-01"},
-            {"descricao":"Bolsa", "valor":"70", "prestacoes":"1", "datavencimento":"2016-07-08"},
-            {"descricao":"Oculos", "valor":"180", "prestacoes":"2", "datavencimento":"2016-07-20"},
-            {"descricao":"Notebook", "valor":"2000", "prestacoes":"10", "datavencimento":"2016-07-15"},
-            {"descricao":"Meia", "valor":"500", "prestacoes":"7", "datavencimento":"2016-09-15"}
+        $scope.contas = [
+            {"descricao":"Tênis", "valor":"90.00", "parcela":"5", "datavencimento":"2017-12-05"},
+            {"descricao":"Mouse Razor", "valor":"150", "parcela":"3", "datavencimento":"2018-01-05"},
+            {"descricao":"Carro", "valor":"450", "parcela":"25", "datavencimento":"2016-07-01"},
+            {"descricao":"Bolsa", "valor":"70", "parcela":"1", "datavencimento":"2016-07-08"},
+            {"descricao":"Oculos", "valor":"180", "parcela":"2", "datavencimento":"2016-07-20"},
+            {"descricao":"Notebook", "valor":"2000", "parcela":"10", "datavencimento":"2016-07-15"},
+            {"descricao":"Meia", "valor":"500", "parcela":"7", "datavencimento":"2016-09-15"}
         ];
 
         $scope.periodos = [
@@ -78,16 +78,16 @@ var cronogramaCtrl = function ($scope, $rootScope, $location, genericAPI) {
         };
         totaisValor();
     
-        $scope.listarDespesaPorUsuario = function () {
+        $scope.listarContaPorUsuario = function () {
             var data = {
                 "metodo":"listarPorUsuario",
-                "class":"despesa"
+                "class":"conta"
             };
             genericAPI.generic(data)
             .then(function successCallback(response) {
                 if( response.data.success === true ){
-                    $scope.despesas = response.data.data;
-                    ordenaDatas( $scope.despesas );
+                    $scope.contas = response.data.data;
+                    ordenaDatas( $scope.contas );
                     montaValorMes();
                 }else{
                     console.log( response.data.msg );
@@ -97,10 +97,10 @@ var cronogramaCtrl = function ($scope, $rootScope, $location, genericAPI) {
             });	
     
         }
-        $scope.listarDespesaPorUsuario();
+        $scope.listarContaPorUsuario();
     
-        function ordenaDatas (despesas) {
-            var datas = despesas;
+        function ordenaDatas (contas) {
+            var datas = contas;
             var array = [];
             while (datas.length > 0) {
                 var num = {"datavencimento":"9999/12/31"};
@@ -115,18 +115,18 @@ var cronogramaCtrl = function ($scope, $rootScope, $location, genericAPI) {
                 array.push(num);
                 datas.splice( pos, 1 );
             }
-            $scope.despesas = array;
+            $scope.contas = array;
         }
         
         // construindo tabelas de mes
     
         function montaValorMes () {
-            // laco de despesas
-            for (var x in $scope.despesas) {
+            // laco de contas
+            for (var x in $scope.contas) {
                 // separo a despesa
-                var despesa = $scope.despesas[x];
-                // crio um atributo pres(array) na despesa
-                despesa.pres = [];
+                var conta = $scope.contas[x];
+                // crio um atributo pres(array) na conta
+                conta.pres = [];
     
                 // laco de meses até 8
                 // for (var m=0; m<8; m++) {
@@ -136,54 +136,36 @@ var cronogramaCtrl = function ($scope, $rootScope, $location, genericAPI) {
                     var year = moment().add(m, "M").year(); // ano atual + 1
                     var data = ''; // variavel data
     
-                    // laco de prestacoes da despesa
-                    for (var p=0; p<despesa.prestacoes; p++) {
-                        var dia = moment(despesa.datavencimento).add(p, "M").date(); // dia da data despesa + index
-                        var mes = moment(despesa.datavencimento).add(p, "M").month(); // mes data despesa + index
-                        var ano = moment(despesa.datavencimento).add(p, "M").year(); // ano data despesa + index
+                    // laco de parcelas da conta
+                    for (var p=0; p<conta.parcela; p++) {
+                        var dia = moment(conta.datavencimento).add(p, "M").date(); // dia da data conta + index
+                        var mes = moment(conta.datavencimento).add(p, "M").month(); // mes data conta + index
+                        var ano = moment(conta.datavencimento).add(p, "M").year(); // ano data conta + index
                         
-                        // caso o mes e ano da despesa seja = ao mes e ano atual 
+                        // caso o mes e ano da conta seja = ao mes e ano atual 
                         if (mes === month && ano === year) {
-                            data = {"data": mes+"/"+ano, "valor":despesa.valor};
+                            data = {"data": mes+"/"+ano, "valor":conta.valor};
                             // pegando a prestação atual
                             if (m === 0) { // m = o ( mes atual )
-                                despesa.prestacao = (p+1)+"/"+despesa.prestacoes;
+                                conta.parcelaAtual = (p+1)+"/"+conta.parcela;
                             }
                         }
                     }
 
-                    // var totalprestacao = despesa.parcelas.length;
-                    // for (var p=0; p<totalprestacao; p++) {
-                    //     var parcela = despesa[p]; // parcela da despesa
-
-                    //     var dia = moment(parcela.datavencimento).date(); // dia da data despesa
-                    //     var mes = moment(parcela.datavencimento).month(); // mes data despesa
-                    //     var ano = moment(parcela.datavencimento).year(); // ano data despesa
-                        
-                    //     // caso o mes e ano da despesa seja = ao mes e ano atual 
-                    //     if (mes === month && ano === year) {
-                    //         data = {"data": mes+"/"+ano, "valor":despesa[p].valor};
-                    //         // pegando a prestação atual
-                    //         if (m === 0) { // m = o ( mes atual )
-                    //             despesa.prestacao = (p+1)+"/"+despesa.prestacoes;
-                    //         }
-                    //     }
-                    // }
-    
-                    if (!despesa.prestacao) {
+                    if (!conta.parcelaAtual) {
                         // se ano = atual mas mes menor ou ano menor
                         if ( (ano === year && mes < month) || (ano < year) ) {
-                            despesa.prestacao = despesa.prestacoes+"/"+despesa.prestacoes;
+                            conta.parcelaAtual = conta.parcela+"/"+conta.parcela;
                         }else{
-                            despesa.prestacao = "0/"+despesa.prestacoes;
+                            conta.parcelaAtual = "0/"+conta.parcela;
                         }
                     }
     
                     if ( data === '' ) {
-                        despesa.pres.push({"data": "00/0000", "valor":"xxxx", "color":"background:#f0f5f5; color:#ccc;", "icon":"fa-trophyx"});
+                        conta.pres.push({"data": "00/0000", "valor":"xxxx", "color":"background:#f0f5f5; color:#ccc;", "icon":"fa-trophyx"});
                     }else{
-                        despesa.pres.push(data);
-                        $scope.totais[m].valor = $scope.totais[m].valor +  parseInt(despesa.valor);
+                        conta.pres.push(data);
+                        $scope.totais[m].valor = $scope.totais[m].valor +  parseInt(conta.valor);
                         
                         if($scope.totais[m-1] != undefined && $scope.totais[m].valor < $scope.totais[m-1].valor) {
                             $scope.totais[m].icon = "fa-arrow-down";
@@ -195,7 +177,7 @@ var cronogramaCtrl = function ($scope, $rootScope, $location, genericAPI) {
                     }
                 }
     
-                $scope.totalgeral = $scope.totalgeral + parseFloat(despesa.valor);
+                $scope.totalgeral = $scope.totalgeral + parseFloat(conta.valor);
             }
         }
     
@@ -203,7 +185,7 @@ var cronogramaCtrl = function ($scope, $rootScope, $location, genericAPI) {
             $scope.meses = [];
             carregaMeses();
             totaisValor();
-            ordenaDatas( $scope.despesas );
+            ordenaDatas( $scope.contas );
             montaValorMes();
         };
         $scope.changeTlMeses();
