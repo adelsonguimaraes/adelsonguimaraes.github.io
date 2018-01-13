513edf161cdeef7e6b3c78ac78d5de49f89d3843
adelsonguimaraes <adelsonguimaraes@gmail.com>
2018-01-13 02:42:09 -0400

    .

--- cfp/libs/js/config.js
+++ cfp/libs/js/config.js
@@ -3,7 +3,7 @@
 	"use strict"
 
 	var config = function ($stateProvider, $urlRouterProvider, $ocLazyLoadProvider, IdleProvider, KeepaliveProvider) {
-		$urlRouterProvider.otherwise("/home");
+		$urlRouterProvider.otherwise("/menu");
 
 		$ocLazyLoadProvider.config({
 	        // Set to true if you want to see what and when is dynamically loaded
