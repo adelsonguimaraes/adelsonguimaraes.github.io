a42ef281ec56a3135361c07384e505fda0b82288
adelsonguimaraes@gmail.com <adelsonguimaraes@gmail.com>
2018-02-09 18:37:16 -0400

    .

--- cfp/libs/js/controllers/homeCtrl.js
+++ cfp/libs/js/controllers/homeCtrl.js
@@ -15,7 +15,7 @@ var homeCtrl = function ($scope, $rootScope, $location, genericAPI) {
 			'feed': {
 				'title':'Alteração de Senha',
 				'text': 'Ao navegar pelo sistema uma verificação de segurança será feita em sua conta para verificar se a senha incial foi atualizada, por motivos de segurança, saiba que é importante fazer a atualização da senha pois muitos usuários receberam senhas inciais, e para evitar a fragilidade do sistema atualize sua senha.',
-				'img': './libs/img/feeds/alterarsenha.png'
+				'img': './libs/img/feeds/alterarsenha.PNG'
 			},
 			'author': {
 				'name':'Sistema de Controle Financeiro Pessoal',
@@ -28,7 +28,7 @@ var homeCtrl = function ($scope, $rootScope, $location, genericAPI) {
 			'feed': {
 				'title':'Meus Dados',
 				'text': 'Ao clicar no nome de usuário é exibido uma janela, onde é possível consultar os dados do usuário, email, nome, a versão do sistema, é possível acionar o botão para alterar a senha e fazer o deslogamento do sistema.',
-				'img': './libs/img/feeds/meusdados.png'
+				'img': './libs/img/feeds/meusdados.PNG'
 			},
 			'author': {
 				'name':'Sistema de Controle Financeiro Pessoal',
@@ -41,7 +41,7 @@ var homeCtrl = function ($scope, $rootScope, $location, genericAPI) {
 			'feed': {
 				'title':'Menu Rodapé',
 				'text': 'A navegação entre os menus se tornou mais simplificada, com o menu no rodapé, tendo em destaque botões para as principais funções do sistema, na ordem da direita para esquerda, Cronograma, Despesa, Recebimentos e Outros.',
-				'img': './libs/img/feeds/menu.png'
+				'img': './libs/img/feeds/menu.PNG'
 			},
 			'author': {
 				'name':'Sistema de Controle Financeiro Pessoal',
@@ -54,7 +54,7 @@ var homeCtrl = function ($scope, $rootScope, $location, genericAPI) {
 			'feed': {
 				'title':'Menu Topo',
 				'text': 'Na parte superior da tela encontra-se o menu de topo, a esquerda a Sigla do sistema que pode ser clicada para ter acesso a tela de Informações, e a direita o nome do usuário que também pode ser clicado para ter acesso a tela de dados do usuário.',
-				'img': './libs/img/feeds/menutopo.png'
+				'img': './libs/img/feeds/menutopo.PNG'
 			},
 			'author': {
 				'name':'Sistema de Controle Financeiro Pessoal',
