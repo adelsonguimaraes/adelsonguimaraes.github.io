c838db37c7a525c0ba0728480b70f23bdaf01e87
adelsonguimaraes@gmail.com <adelsonguimaraes@gmail.com>
2018-05-02 14:24:58 -0400

    .

--- estudandopwa/service-worker.js
+++ estudandopwa/service-worker.js
@@ -2,16 +2,16 @@ importScripts('./libs/js/sw-cache-polyfill.js');
 
 let cacheName = 'estudandopwa-v.1.0.0';
 let filesToCache = [
-    './',
-    'index.html',
-    'libs/css/style.css',
-    'libs/js/util/notification.js'
+    // './',
+    // 'index.html',
+    // 'libs/css/style.css',
+    // 'libs/js/util/notification.js'
 ];
 
 self.addEventListener('install', (e) => {
     console.log( '[ServiceWorker] Installer' );
     // forçando service atualizar
-    // self.skipWaiting();
+    self.skipWaiting();
     e.waitUntil(
         caches.open(cacheName).then((cache) =>{
             // console.log( '[ServiceWorker] Caching app shell' );
