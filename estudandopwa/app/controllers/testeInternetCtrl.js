91663d268d3c954b6e9632f6d20fd93fc133e3ec
adelsonguimaraes <adelsonguimaraes@gmail.com>
2018-05-06 13:55:55 -0400

    .

--- estudandopwa/app/controllers/testeInternetCtrl.js
+++ estudandopwa/app/controllers/testeInternetCtrl.js
@@ -3,16 +3,20 @@ var div = document.getElementById("statusnet");
 var p = div.querySelector('p');
 var figure = document.getElementById("imgstatus");
 var img = figure.querySelector('img');
+var toast = document.getElementById("toast");
+
 // function que funciona como escuta do evento
 function setStatusNet(e) {
     if(navigator.onLine) {
         p.innerHTML = "Deslige a internet para testar!";
         img.src = "http://sambandocomevelyn.com.br/wp-content/uploads/2016/12/selo-online.png";
         img.height = "80";
+        toast.style.display = "none";
     }else{
         p.innerHTML = "Ligue a internet para testar!";
-        img.src = "https://previews.123rf.com/images/pockygallery/pockygallery1504/pockygallery150400479/39166665-offline-red-stamp-text-on-white.jpg";
+        img.src = "libs/img/off-line.jpg";
         img.height = "140";
+        toast.style.display = "block";
     }
 }
 
