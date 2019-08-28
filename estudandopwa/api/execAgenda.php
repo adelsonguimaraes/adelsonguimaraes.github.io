45afc6bc713546d71a2d7883e9cf602016b88dea
Adelson Guimarães <adelsonguimaraes@gmail.com>
2019-08-28 01:37:33 -0400

    .

--- estudandopwa/api/execAgenda.php
+++ estudandopwa/api/execAgenda.php
@@ -8,14 +8,16 @@
     $script_tz = date_default_timezone_get();
 
     $agendas = array(
-        "data" => "2019-08-26",
-        "horario" => "15:40:00",
-        "notification" => array(
-            "id" => 1,
-            "headings" => array("en" => "Agendamento hoje 26/08/2019 às 16:00h"),
-            "subtitle" => array("en" => "Reunião Agendada com Senhor Marcos"),
-            "contents" => array("en" => "Reunião com intuito de verficar as necessidades do cliente com seu investimento."),
-            "data" => null
+        array(
+            "data" => "2019-08-26",
+            "horario" => "15:40:00",
+            "notification" => array(
+                "id" => 1,
+                "headings" => array("en" => "Agendamento hoje 26/08/2019 às 16:00h"),
+                "subtitle" => array("en" => "Reunião Agendada com Senhor Marcos"),
+                "contents" => array("en" => "Reunião com intuito de verficar as necessidades do cliente com seu investimento."),
+                "data" => null
+            )
         ),
         array(
             "data" => "2019-08-26",
@@ -46,8 +48,8 @@
     $datahorario = new DateTime($agenda['data'] . ' ' . $agenda['horario']);
     $dataagora = new DateTime();
     $interval = $datahorario->diff($dataagora);
-    
-    if ($interval->format("%I") < 60) {
+
+    // if ($interval->format("%I") < 60 && $interval->format("%D") === "00" && $interval->format("%H") === "00") {
         $response = sendMessage($agenda["notification"]);
-    }
+    // }
 ?>
